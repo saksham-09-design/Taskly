d2df5c5ea12942116f964ba4ea435bc97aa159e6
Saksham <sakshamloona9@gmail.com>
2024-12-17 23:17:21 +0530

    Added Packages and Stateful Widgets

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage();
+
+  @override
+  State<StatefulWidget> createState() {
+    return _HomePageClass();
+  }
+}
+
+class _HomePageClass extends State<HomePage> {
+  _HomePageClass();
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
