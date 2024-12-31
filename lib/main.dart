f15d542355df4eb1957fedf61f581871f0eb2d2e
Saksham <sakshamloona9@gmail.com>
2024-12-31 20:54:16 +0530

    Initialized Hive and Created Task Class

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/adapters.dart';
 import 'package:taskly/pages/home_page.dart';
 
-void main() {
+void main() async {
+  await Hive.initFlutter("hive_Pages");
   runApp(const MyApp());
 }
 
